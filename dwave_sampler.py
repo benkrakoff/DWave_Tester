e210f9c038b650808100ec48b487235b9a96a41f
Benjamin Krakoff <bkrakoff@umich.edu>
2020-11-08 00:18:14 +0000

    Clique experiment, interrupted because time expired

--- dwave_sampler.py
+++ dwave_sampler.py
@@ -5,6 +5,7 @@
 
 from dwave.system import DWaveSampler, EmbeddingComposite
 import dwave_networkx as dnx
+import networkx as nx
 import numpy as np
 import pandas as pd
 rng = np.random.default_rng()
@@ -12,9 +13,9 @@ import time
 
 edge_weights = {}
 
-G = dnx.pegasus_graph(2)
+G = nx.complete_graph(40)
 
-for i in range(4):
+for i in range(5):
 
     for e in G.edges():
         edge_weights[e] = 2*rng.binomial(1, .5)-1
@@ -29,15 +30,15 @@ for i in range(4):
                 col.append(0)
         H[n] = col
  
-    pd.DataFrame.from_dict(H).to_csv("Hamiltonian_{k}".format(k = i+2))
+    pd.DataFrame.from_dict(H).to_csv("Clique_Hamiltonian_{k}".format(k = i+1))
 
     t_1 = time.time()
     sampleset = EmbeddingComposite(DWaveSampler()).sample_ising({}, edge_weights, num_reads = 1000)
     t_2 = time.time()
 
-    sampleset.to_pandas_dataframe().to_csv("Sampler_Data_{k}".format(k=i+2))
+    sampleset.to_pandas_dataframe().to_csv("Clique_Sampler_Data_{k}".format(k=i+1))
 
     time_data = pd.read_csv("Time_Data")
-    time_data.insert(len(time_data.columns), "Hamiltonian_{k}".format(k = i+2), t_2 - t_1)
+    time_data.insert(len(time_data.columns), "Clique_Hamiltonian_{k}".format(k = i+1), t_2 - t_1)
     time_data.to_csv("Time_Data")
 
--- embedding.py
+++ embedding.py
@@ -2,25 +2,25 @@ import dwave_networkx as dnx
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import networkx as nx
 rng = np.random.default_rng()
 
 edge_weights = {}
 
-G = dnx.pegasus_graph(2)
+G = nx.complete_graph(40)
 
 for e in G.edges():
     edge_weights[e] = 2*rng.binomial(1, .5)-1
 
-H1 = {}
+H = {}
 
 for n in G.nodes():
-    col = []
-    for m in G.nodes():
-        if (n, m) in G.edges():
-            col.append(edge_weights[(min(n, m), max(n, m))])
-        else:
-            col.append(0)
-    H1[n] = col
+        col = []
+        for m in G.nodes():
+            if (n, m) in G.edges():
+                col.append(edge_weights[(min(n, m), max(n, m))])
+            else:
+                col.append(0)
+        H[n] = col
 
-times = {"Hamiltonian_1":[0]}
-pd.DataFrame.from_dict(times).to_csv("Time_Data")
+pd.DataFrame.from_dict(H).to_csv("Test Clique Hamiltonian")
