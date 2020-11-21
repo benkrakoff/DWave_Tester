2c8bbb8160c93ca7aebfd35a2a60046e3e000fed
Benjamin Krakoff <bkrakoff@umich.edu>
2020-11-21 17:53:51 +0000

    Sparse Gaussians

--- dwave_sampler.py
+++ dwave_sampler.py
@@ -12,13 +12,14 @@ rng = np.random.default_rng()
 import time
 
 edge_weights = {}
-#G = dnx.pegasus_graph(5)
-G = nx.complete_graph(119)
+G = dnx.pegasus_graph(5)
+#G = nx.complete_graph(119)
 
-for i in range(5):
+for i in range(6, 11):
 
     for e in G.edges():
-        edge_weights[e] = 2*rng.binomial(1, .5)-1
+        #edge_weights[e] = 2*rng.binomial(1, .5)-1
+        edge_weights[e] = rng.standard_normal()
     H = {}
 
     for n in G.nodes():
@@ -30,18 +31,18 @@ for i in range(5):
                 col.append(0)
         H[n] = col
  
-    pd.DataFrame.from_dict(H).to_csv("Clique_Hamiltonian_{k}".format(k = i+6))
+    pd.DataFrame.from_dict(H).to_csv("Gauss_Hamiltonian_{k}".format(k = i))
 
     t_1 = time.time()
     #sampler for sparse graphs
-    #sampleset = EmbeddingComposite(DWaveSampler()).sample_ising({}, edge_weights, num_reads = 1000)
+    sampleset = EmbeddingComposite(DWaveSampler()).sample_ising({}, edge_weights, num_reads = 1000)
     #sampler for clique graphs
-    sampleset = DWaveCliqueSampler().sample_ising({}, edge_weights, num_reads = 1000)
+    #sampleset = DWaveCliqueSampler().sample_ising({}, edge_weights, num_reads = 1000)
     t_2 = time.time()
 
-    sampleset.to_pandas_dataframe().to_csv("Clique_Sampler_Data_{k}".format(k=i+6))
+    sampleset.to_pandas_dataframe().to_csv("Gauss_Sampler_Data_{k}".format(k=i))
 
     time_data = pd.read_csv("Time_Data")
-    time_data.insert(len(time_data.columns), "Clique_Hamiltonian_{k}".format(k = i+6), t_2 - t_1)
+    time_data["Gauss_Hamiltonian_{k}".format(k = i)] = [t_2 - t_1]
     time_data.to_csv("Time_Data")
 
